857474c2fe0a1ed31fe88d9666e5cba494608878
mateocapon <mcapon@fi.uba.ar>
2020-07-02 23:27:27 -0300

    Agrego comentarios.

--- carga.cpp
+++ carga.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Constructor
 Carga::Carga(Lista<Pelicula>* vistas, Lista<Pelicula>* no_vistas, Lista<Pelicula>* recomendadas, string archivo_vistas, string archivo_no_vistas, string archivo_recomendadas){
 
     this->vistas = vistas;
@@ -14,9 +15,11 @@ Carga::Carga(Lista<Pelicula>* vistas, Lista<Pelicula>* no_vistas, Lista<Pelicula
     this->nombre_archivo_recomendadas = archivo_recomendadas;
 }
 
+//Destructor
 Carga::~Carga(){
 }
 
+//Lee una pelicula y la guarda en memoria dinamica apuntada por peli.
 void Carga::leer_pelicula(ifstream &archivo, Pelicula* &peli){
     string nombre, genero, director, *actor_aux, puntaje_str, linea_en_blanco;
     Lista<string>* actores = new Lista<string>;
@@ -38,6 +41,7 @@ void Carga::leer_pelicula(ifstream &archivo, Pelicula* &peli){
     peli = new Pelicula(nombre, genero, puntaje, director, actores);
 }
 
+// Se agregan las peliculas del archivo en la lista de peliculas.
 void Carga::agregar(Lista<Pelicula>* peliculas, ifstream& archivo){
     Pelicula* aux;
     while(!archivo.eof()){
@@ -46,7 +50,7 @@ void Carga::agregar(Lista<Pelicula>* peliculas, ifstream& archivo){
     }
 }
 
-
+//Si analiza cada pelicula de la lista de no_vistas y si es recomendada, se la agrega a la lista de recomendadas.
 void Carga::agregar_recomendadas(){
     Pelicula* aux;
     for(int i = 1; i <= no_vistas->obtener_tam(); i++){
@@ -57,6 +61,7 @@ void Carga::agregar_recomendadas(){
     }
 }
 
+//Se agregan las peliculas de los archivos nombre_archivo_vistas y nombre_archivo_no_vistas en las respectivas listas.
 void Carga::cargar_datos(){
 
     ifstream archivo_vistas(nombre_archivo_vistas);
--- carga.h
+++ carga.h
@@ -5,23 +5,51 @@
 #include "pelicula.h"
 
 class Carga {
-private:
-    Lista<Pelicula>* vistas;
-    Lista<Pelicula>* no_vistas;
-    Lista<Pelicula>* recomendadas;
-    std::string nombre_archivo_vistas;
-    std::string nombre_archivo_no_vistas;
-    std::string nombre_archivo_recomendadas;
 
-public:
-    Carga(Lista<Pelicula>* vistas, Lista<Pelicula>* no_vistas, Lista<Pelicula>* recomendadas, std::string archivo_vistas, std::string archivo_no_vistas, std::string archivo_recomendadas);
-    ~Carga();
-    void cargar_datos();
+    private:
 
-private:
-    void agregar(Lista<Pelicula>* peliculas, std::ifstream& archivo);
-    void agregar_recomendadas();
-    void leer_pelicula(std::ifstream &archivo, Pelicula* &peli);
+        Lista<Pelicula>* vistas;
+        Lista<Pelicula>* no_vistas;
+        Lista<Pelicula>* recomendadas;
+        std::string nombre_archivo_vistas;
+        std::string nombre_archivo_no_vistas;
+        std::string nombre_archivo_recomendadas;
+
+    public:
+
+        /* Constructor Carga
+        *  Post: Se inicializa cada atributo de la Carga con su correspondiente parametro recibido.
+        */
+        Carga(Lista<Pelicula>* vistas, Lista<Pelicula>* no_vistas, Lista<Pelicula>* recomendadas, std::string archivo_vistas, std::string archivo_no_vistas, std::string archivo_recomendadas);
+
+        //Destructor
+        ~Carga();
+
+        /*
+        * Pre: Los archivos siguen una estructura determinada por nombre, genero, puntaje, director y finalmente una lista de actores.
+        * Post: Se agregan las peliculas de los archivos nombre_archivo_vistas y nombre_archivo_no_vistas en las respectivas listas.
+        */
+        void cargar_datos();
+
+    private:
+        /*
+        * Pre: El archivo sigue una estructura determinada por nombre, genero, puntaje, director y finalmente una lista de actores.
+        * Post: Se agregan las peliculas del archivo en la lista de peliculas.
+        */
+        void agregar(Lista<Pelicula>* peliculas, std::ifstream& archivo);
+
+        /*
+        * Pre: El archivo de recomendadas se abrio correctamente.
+        * Post: Se comparan las peliculas de la lista de no_vistas y si [hay coincidencia en el género y (en el director o por lo menos
+        *       uno de los actores)] o [el puntaje es 7 o más] se agrega a la lista de recomendadas.
+        */
+        void agregar_recomendadas();
+
+         /*
+         * Pre: El archivo fue abierto correctamente y sigue una estructura determinada por nombre, genero, puntaje, director y finalmente una lista de actores.
+         * Post: Lee una pelicula y la guarda en memoria dinamica apuntada por peli.
+         */
+         void leer_pelicula(std::ifstream &archivo, Pelicula* &peli);
 };
 
 
